fb85b1439039b91bebeeee1e09c018f67d05255e
Vasant Goswami <54198208+VasantG@users.noreply.github.com>
2019-11-27 15:37:16 -0600

    Initial Change

---
+++ turtleimage.py
@@ -0,0 +1,13 @@
+# The program generates a star like image.
+
+import turtle
+
+NUM_TRIANGLES = 20
+turtle.forward(200)
+turtle.left(240)
+
+for x in range(NUM_TRIANGLES):
+    turtle.forward(150)
+    turtle.left(190)
+
+
